1a2c2b361d7e4e44b2591acc60d5655511bbd79b
Mateusz Ostróżka <154000222+Mateusz-34@users.noreply.github.com>
2025-10-20 14:15:55 +0200

    Update Product.java

--- src/Product.java
+++ src/Product.java
@@ -9,5 +9,9 @@ public abstract class Product {
         this.price = price;
     }
 
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public String getId() { return id; }
+
     public abstract String getInfo();
 }
